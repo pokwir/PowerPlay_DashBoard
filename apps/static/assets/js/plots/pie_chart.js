2a4086f3477edc3124356dcc2902c5ed7c4ece9b
Patrick Okwir - Macbook <career@okwir.com>
2023-07-25 15:17:24 -0400

    ethnicity chart

--- apps/static/assets/js/plots/pie_chart.js
+++ apps/static/assets/js/plots/pie_chart.js
@@ -1,30 +1,54 @@
 // echarts_pie_chart.js
 
 // Initialize the ECharts instance
-var chartDom = document.getElementById("chart-container");
-var myChart = echarts.init(chartDom);
+var dom = document.getElementById('chart-container');
+var myChart = echarts.init(dom, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
 
-// Configuration for the pie chart
-var option = {
+var option;
+
+option = {
   legend: {
     show: false,
+    top: false,
+  },
+
+  aria: {
+    enabled: true,
+    decal: {
+        show: false
+    }
+},
+  toolbox: {
+    show: false,
+    feature: {
+      mark: { show: true },
+      dataView: { show: true, readOnly: false },
+      restore: { show: true },
+      saveAsImage: { show: true }
+    }
   },
   series: [
     {
-      name: "Ethinicity",
-      type: "pie",
-      radius: [10, 100],
-      center: ["50%", "50%"],
-      roseType: "area",
+      name: '',
+      type: 'pie',
+      radius: [10, 105],
+      center: ['50%', '50%'],
+      roseType: 'area',
       itemStyle: {
-        borderRadius: 15,
+        borderRadius: 8
       },
+      labelLine: {show: false},
+      label: {show: false},
       data: [
-        { value: 40, name: "Black" },
+        { value: 50, name: "Black" },
         { value: 38, name: "White" },
         { value: 32, name: "Latina" },
         { value: 30, name: "Indian" },
-        { value: 28, name: "Asian" },
+        { value: 68, name: "Asian" },
         { value: 26, name: "Other" },
       ],
     },
@@ -38,3 +62,8 @@ var option = {
 
 // Set the configuration and render the chart
 myChart.setOption(option);
+if (option && typeof option === 'object') {
+  myChart.setOption(option);
+}
+
+window.addEventListener('resize', myChart.resize);
