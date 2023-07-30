59bf9c8deb18a18ecfd942d3e72645285b07f7ee
Patrick Okwir - Macbook <career@okwir.com>
2023-07-29 23:36:07 -0400

    grouped bar charts

---
+++ apps/static/assets/js/plots/grouped_bar.js
@@ -0,0 +1,88 @@
+var ctx = document.getElementById("chart-grouped-bars").getContext("2d");
+
+new Chart(ctx, {
+  type: "bar",
+  render: "canvas",
+  data: {
+    labels: ["Grant Avenue", "FDA", "449X"],
+    datasets: [
+      {
+        label: "Boys",
+        backgroundColor: "rgb(255, 112, 112)", // Red
+        data: [55, 83, 65],
+        maxBarThickness: 50,
+        tension: 0.4,
+        borderWidth: 0,
+        borderRadius: 10,
+        borderSkipped: false,
+      },
+      {
+        label: "Girls",
+        backgroundColor: "rgb(93, 123, 217)", // Purple
+        data: [59, 79, 75],
+        maxBarThickness: 50,
+        tension: 0.4,
+        borderWidth: 0,
+        borderRadius: 10,
+        borderSkipped: false,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    interaction: {
+      intersect: false,
+      mode: "index",
+    },
+    scales: {
+      y: {
+        grid: {
+          drawBorder: true,
+          display: false,
+          drawOnChartArea: false,
+          drawTicks: false,
+        },
+        ticks: {
+          suggestedMin: 0,
+          suggestedMax: 500,
+          beginAtZero: true,
+          padding: 15,
+          font: {
+            size: 10,
+            family: "Open Sans",
+            style: "normal",
+            lineHeight: 2,
+          },
+          color: "blue",
+        },
+      },
+      x: {
+        grid: {
+          drawBorder: false,
+          display: false,
+          drawOnChartArea: false,
+          drawTicks: false,
+        },
+        ticks: {
+          suggestedMin: 0,
+          suggestedMax: 500,
+          beginAtZero: true,
+          padding: 15,
+          font: {
+            size: 10,
+            family: "Open Sans",
+            style: "normal",
+            lineHeight: 2,
+          },
+          color: "blue",
+        },
+      },
+    },
+  },
+});
