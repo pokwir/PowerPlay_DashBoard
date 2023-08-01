3be1ec2e59f884018b08f3f5310b630eedc634c8
Patrick Okwir - Macbook <career@okwir.com>
2023-07-31 23:29:19 -0400

    billing

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ from apps.config import config_dict
 from apps import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+DEBUG = (os.getenv('DEBUG', 'True') == 'True')
 
 # The configuration
 get_config_mode = 'Debug' if DEBUG else 'Production'
